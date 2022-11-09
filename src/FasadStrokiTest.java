0e0d00366b52bef49c92fe83d9b7194099000df8
Lenovo <123@gmail.com>
2022-11-09 10:59:07 +0300

    +FasadTest

--- src/FasadStrokiTest.java
+++ src/FasadStrokiTest.java
@@ -5,11 +5,11 @@ public class FasadStrokiTest {
 
     @Test
     public void result() {
-        StrokiAdd stroki = new StrokiAdd("Я учусь в ", "БГУ");
-        StrokiAddN strokiAddN = new StrokiAddN(", Я и Бал БГУ", 3);
+        StrokiAdd stroki = new StrokiAdd("Я учусь в ", "БГУ, на");
+        StrokiAddN strokiAddN = new StrokiAddN(" ФПМИ,", 3);
         FasadStroki fas = new FasadStroki(stroki, strokiAddN);
         fas.result();
         String res = Struct.s1 + Struct.s2;
-        Assert.assertEquals(res, "Я учусь в БГУ, Я и Бал БГУ, Я и Бал БГУ, Я и Бал БГУ");
+        Assert.assertEquals(res, "Я учусь в БГУ, на ФПМИ, ФПМИ, ФПМИ,");
     }
 }
